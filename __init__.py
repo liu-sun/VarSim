3788a5c47b814b2c311ba1d6681c9ab632d751ba
liu-sun <sunliu@yxnu.edu.cn>
2023-09-12 11:49:08 +0800

    simulate
    simulate MANE select CDS sequence from NCBI

---
+++ __init__.py
@@ -0,0 +1,39 @@
+import configparser
+
+from Bio import Entrez, SeqIO
+from Bio.Seq import Seq
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+Entrez.email = config["DEFAULT"]["email"]
+Entrez.api_key = config["DEFAULT"]["api_key"]
+print(Entrez.email, Entrez.api_key)
+
+
+def simulate(gene):
+    handle = Entrez.esearch(
+        db="nucleotide", term=f'{gene}[gene] AND "mane select"[keyword]')
+    record = Entrez.read(handle)
+    idlist = record["IdList"]
+    handle = Entrez.efetch(db="nucleotide", id=idlist,
+                           rettype="gb", retmode="text")
+    seqrecord = SeqIO.read(handle, "genbank")
+    for feature in seqrecord.features:
+        if feature.type == "CDS":
+            protein = "".join(feature.qualifiers.get("translation"))
+            cds = feature.location.extract(seqrecord).seq
+    for index, codon in enumerate(range(0, len(cds) - 3, 3)):
+        for base in "ATCG":
+            if base != cds[codon]:
+                seq = Seq(base) + cds[codon + 1: codon + 3]
+                print(protein[index], index + 1, seq.translate(), sep="")
+            if base != cds[codon + 1]:
+                seq = cds[codon] + Seq(base) + cds[codon + 2]
+                print(protein[index], index + 1, seq.translate(), sep="")
+            if base != cds[codon + 2]:
+                seq = cds[codon: codon + 2] + Seq(base)
+                print(protein[index], index + 1, seq.translate(), sep="")
+
+
+if __name__ == "__main__":
+    simulate("SLC22A5")
