ce1f9046310ed9c1f6bfd7c8b041c3d3ca7b8198
liu-sun <sunliu@yxnu.edu.cn>
2023-09-13 20:36:19 +0800

    c dot & p dot
    c dot and p dot

--- __init__.py
+++ __init__.py
@@ -11,27 +11,30 @@ Entrez.api_key = config["DEFAULT"]["api_key"]
 
 def simulate(gene):
     handle = Entrez.esearch(
-        db="nucleotide", term=f'{gene}[gene] AND "mane select"[keyword]')
+        db='nucleotide', term=f'{gene}[gene] "mane select"[keyword]')
     record = Entrez.read(handle)
     idlist = record["IdList"]
-    handle = Entrez.efetch(db="nucleotide", id=idlist,
-                           rettype="gb", retmode="text")
-    seqrecord = SeqIO.read(handle, "genbank")
+    handle = Entrez.efetch(db='nucleotide', id=idlist,
+                           rettype='gb', retmode='text')
+    seqrecord = SeqIO.read(handle, 'genbank')
     for feature in seqrecord.features:
         if feature.type == "CDS":
             protein = "".join(feature.qualifiers.get("translation"))
             cds = feature.location.extract(seqrecord).seq
-    for index, codon in enumerate(range(0, len(cds) - 3, 3)):
+    for index, codon in enumerate(range(0, len(cds)-3, 3)):
         for base in "ATCG":
             if base != cds[codon]:
-                seq = Seq(base) + cds[codon + 1: codon + 3]
-                print(protein[index], index + 1, seq.translate(), sep="")
-            if base != cds[codon + 1]:
-                seq = cds[codon] + Seq(base) + cds[codon + 2]
-                print(protein[index], index + 1, seq.translate(), sep="")
-            if base != cds[codon + 2]:
-                seq = cds[codon: codon + 2] + Seq(base)
-                print(protein[index], index + 1, seq.translate(), sep="")
+                seq = Seq(base)+cds[codon+1:codon+3]
+                print("c.", codon+1, cds[codon], ">", base, " ", "p.",
+                      protein[index], index+1, seq.translate(), sep="")
+            if base != cds[codon+1]:
+                seq = cds[codon]+Seq(base)+cds[codon+2]
+                print("c.", codon+2, cds[codon+1], ">", base, " ", "p.",
+                      protein[index], index+1, seq.translate(), sep="")
+            if base != cds[codon+2]:
+                seq = cds[codon:codon+2]+Seq(base)
+                print("c.", codon+3, cds[codon+2], ">", base, " ", "p.",
+                      protein[index], index+1, seq.translate(), sep="")
 
 
 if __name__ == "__main__":
