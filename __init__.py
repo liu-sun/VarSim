53eb8bd877d63a18d7c5eeb44f9fdd48744d7437
liu-sun <sunliu@yxnu.edu.cn>
2023-09-14 18:57:14 +0800

    hgvsc & hgvsp
    hgvsc and hgvsp

--- __init__.py
+++ __init__.py
@@ -20,21 +20,22 @@ def simulate(gene):
     for feature in seqrecord.features:
         if feature.type == "CDS":
             protein = "".join(feature.qualifiers.get("translation"))
+            protein_id = "".join(feature.qualifiers.get('protein_id'))
             cds = feature.location.extract(seqrecord).seq
     for index, codon in enumerate(range(0, len(cds)-3, 3)):
         for base in "ATCG":
             if base != cds[codon]:
                 seq = Seq(base)+cds[codon+1:codon+3]
-                print("c.", codon+1, cds[codon], ">", base, " ", "p.",
-                      protein[index], index+1, seq.translate(), sep="")
+                print(f"{seqrecord.id}:c.{codon+1}{cds[codon]}>{base}",
+                      f"{protein_id}:p.{protein[index]}{index+1}{seq.translate()}")
             if base != cds[codon+1]:
                 seq = cds[codon]+Seq(base)+cds[codon+2]
-                print("c.", codon+2, cds[codon+1], ">", base, " ", "p.",
-                      protein[index], index+1, seq.translate(), sep="")
+                print(f"{seqrecord.id}:c.{codon+2}{cds[codon+1]}>{base}",
+                      f"{protein_id}:p.{protein[index]}{index+1}{seq.translate()}")
             if base != cds[codon+2]:
                 seq = cds[codon:codon+2]+Seq(base)
-                print("c.", codon+3, cds[codon+2], ">", base, " ", "p.",
-                      protein[index], index+1, seq.translate(), sep="")
+                print(f"{seqrecord.id}:c.{codon+3}{cds[codon+2]}>{base}",
+                      f"{protein_id}:p.{protein[index]}{index+1}{seq.translate()}")
 
 
 if __name__ == "__main__":
