e06745cfef332847d30e30a35438b908e4bb3b60
liu-sun <sunliu@yxnu.edu.cn>
2024-01-22 18:07:02 +0800

    update email & api_key
    email & api_key from os environ

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# VariantSimulator
-Variant Simulator
+# VariationSimulation
+VariationSimulation generates simulations for all possible single nucleotide variants (SNVs) for the Matched Annotation from NCBI and EMBL-EBI (MANE) transcript, along with the corresponding protein using HGVS notation.
+
+**INSTALLATION**
+```powershell
+pip install variationsimulation
+```
+Variation Simulation
+
+*input*: HGNC symbol
+
+*output*: HGVSc and HGVSp notation
--- __init__.py
+++ __init__.py
@@ -1,42 +1,56 @@
-import configparser
+import os
 
 from Bio import Entrez, SeqIO
 from Bio.Seq import Seq
 
-config = configparser.ConfigParser()
-config.read("config.ini")
-Entrez.email = config["DEFAULT"]["email"]
-Entrez.api_key = config["DEFAULT"]["api_key"]
+Entrez.email = os.environ["EMAIL"]
+Entrez.api_key = os.environ["API_KEY"]
 
 
 def simulate(gene):
+    variants = []
     handle = Entrez.esearch(
-        db='nucleotide', term=f'{gene}[gene] "mane select"[keyword]')
+        db="nucleotide", term=f'{gene}[gene] "mane select"[keyword]'
+    )
     record = Entrez.read(handle)
     idlist = record["IdList"]
-    handle = Entrez.efetch(db='nucleotide', id=idlist,
-                           rettype='gb', retmode='text')
-    seqrecord = SeqIO.read(handle, 'genbank')
+    handle = Entrez.efetch(db="nucleotide", id=idlist, rettype="gb", retmode="text")
+    seqrecord = SeqIO.read(handle, "genbank")
     for feature in seqrecord.features:
         if feature.type == "CDS":
             protein = "".join(feature.qualifiers.get("translation"))
-            protein_id = "".join(feature.qualifiers.get('protein_id'))
+            protein_id = "".join(feature.qualifiers.get("protein_id"))
             cds = feature.location.extract(seqrecord).seq
-    for index, codon in enumerate(range(0, len(cds)-3, 3)):
+    for index, codon in enumerate(range(0, len(cds) - 3, 3)):
         for base in "ATCG":
             if base != cds[codon]:
-                seq = Seq(base)+cds[codon+1:codon+3]
-                print(f"{seqrecord.id}:c.{codon+1}{cds[codon]}>{base}",
-                      f"{protein_id}:p.{protein[index]}{index+1}{seq.translate()}")
-            if base != cds[codon+1]:
-                seq = cds[codon]+Seq(base)+cds[codon+2]
-                print(f"{seqrecord.id}:c.{codon+2}{cds[codon+1]}>{base}",
-                      f"{protein_id}:p.{protein[index]}{index+1}{seq.translate()}")
-            if base != cds[codon+2]:
-                seq = cds[codon:codon+2]+Seq(base)
-                print(f"{seqrecord.id}:c.{codon+3}{cds[codon+2]}>{base}",
-                      f"{protein_id}:p.{protein[index]}{index+1}{seq.translate()}")
+                seq = Seq(base) + cds[codon + 1 : codon + 3]
+                variants.append(
+                    (
+                        f"{seqrecord.id}:c.{codon+1}{cds[codon]}>{base}",
+                        f"{protein_id}:p.{protein[index]}{index+1}{seq.translate()}",
+                    )
+                )
+            if base != cds[codon + 1]:
+                seq = cds[codon] + Seq(base) + cds[codon + 2]
+                variants.append(
+                    (
+                        f"{seqrecord.id}:c.{codon+2}{cds[codon+1]}>{base}",
+                        f"{protein_id}:p.{protein[index]}{index+1}{seq.translate()}",
+                    )
+                )
+            if base != cds[codon + 2]:
+                seq = cds[codon : codon + 2] + Seq(base)
+                variants.append(
+                    (
+                        f"{seqrecord.id}:c.{codon+3}{cds[codon+2]}>{base}",
+                        f"{protein_id}:p.{protein[index]}{index+1}{seq.translate()}",
+                    )
+                )
+    return variants
 
 
 if __name__ == "__main__":
-    simulate("SLC22A5")
+    import pprint
+
+    pprint.pprint(simulate("SLC22A5"))
