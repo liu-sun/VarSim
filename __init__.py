ff48c0ef22faa61049fdbd820a06b7d2b5ec005c
sunliu <sunliu@picb.ac.cn>
2024-06-25 21:51:41 +0800

    add 5' UTR and 3' UTR SNV
    add 5' UTR and 3' UTR SNV

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ from Bio import Entrez, SeqIO
 from Bio.Data.IUPACData import unambiguous_dna_letters, protein_letters, protein_letters_1to3
 from Bio.Seq import Seq
 from Bio.SeqUtils import seq3
+from Bio.SeqFeature import SimpleLocation
 
 Entrez.email = os.environ["EMAIL"]
 Entrez.api_key = os.environ["API_KEY"]
@@ -16,55 +17,84 @@ codons = (
 
 def snv(gene: str) -> list:
     variants = []
-    handle = Entrez.esearch(db="nucleotide", term=f'{gene}[gene] "mane select"[keyword]')
+    handle = Entrez.esearch(db="nucleotide", term=f'{
+                            gene}[gene] "mane select"[keyword]')
     record = Entrez.read(handle)
-    handle = Entrez.efetch(db="nucleotide", id=record["IdList"], rettype="gb", retmode="text")
+    handle = Entrez.efetch(
+        db="nucleotide", id=record["IdList"], rettype="gb", retmode="text")
     seqrecord = SeqIO.read(handle, "genbank")
     for feature in seqrecord.features:
         if feature.type == "CDS":
             protein = "".join(feature.qualifiers.get("translation"))
             protein_id = "".join(feature.qualifiers.get("protein_id"))
             cds = feature.location.extract(seqrecord).seq
+            utr5 = SimpleLocation(
+                0, feature.location.start).extract(seqrecord).seq
+            utr3 = SimpleLocation(feature.location.end, len(
+                seqrecord)).extract(seqrecord).seq
+# coding region
     for index, codon in enumerate(range(0, len(cds) - 3, 3)):
         for base in unambiguous_dna_letters:
             if base != cds[codon]:
                 seq = Seq(base) + cds[codon + 1: codon + 3]
                 if protein[index] != seq.translate():
                     variants.append((f"{seqrecord.id}:c.{codon + 1}{cds[codon]}>{base}",
-                                     f"{protein_id}:p.{protein[index]}{index + 1}{seq.translate()}",
+                                     f"{protein_id}:p.{protein[index]}{
+                                         index + 1}{seq.translate()}",
                                      f"{protein_id}:p.{seq3(protein[index])}{index + 1}{seq3(seq.translate())}",))
                 else:
                     variants.append((f"{seqrecord.id}:c.{codon + 1}{cds[codon]}>{base}",
-                                     f"{protein_id}:p.{protein[index]}{index + 1}=",
+                                     f"{protein_id}:p.{
+                                         protein[index]}{index + 1}=",
                                      f"{protein_id}:p.{seq3(protein[index])}{index + 1}=",))
             if base != cds[codon + 1]:
                 seq = cds[codon] + Seq(base) + cds[codon + 2]
                 if protein[index] != seq.translate():
                     variants.append((f"{seqrecord.id}:c.{codon + 2}{cds[codon + 1]}>{base}",
-                                     f"{protein_id}:p.{protein[index]}{index + 1}{seq.translate()}",
+                                     f"{protein_id}:p.{protein[index]}{
+                                         index + 1}{seq.translate()}",
                                      f"{protein_id}:p.{seq3(protein[index])}{index + 1}{seq3(seq.translate())}",))
                 else:
                     variants.append((f"{seqrecord.id}:c.{codon + 2}{cds[codon + 1]}>{base}",
-                                     f"{protein_id}:p.{protein[index]}{index + 1}=",
+                                     f"{protein_id}:p.{
+                                         protein[index]}{index + 1}=",
                                      f"{protein_id}:p.{seq3(protein[index])}{index + 1}=",))
             if base != cds[codon + 2]:
                 seq = cds[codon: codon + 2] + Seq(base)
                 if protein[index] != seq.translate():
                     variants.append((f"{seqrecord.id}:c.{codon + 3}{cds[codon + 2]}>{base}",
-                                     f"{protein_id}:p.{protein[index]}{index + 1}{seq.translate()}",
+                                     f"{protein_id}:p.{protein[index]}{
+                                         index + 1}{seq.translate()}",
                                      f"{protein_id}:p.{seq3(protein[index])}{index + 1}{seq3(seq.translate())}",))
                 else:
                     variants.append((f"{seqrecord.id}:c.{codon + 3}{cds[codon + 2]}>{base}",
-                                     f"{protein_id}:p.{protein[index]}{index + 1}=",
+                                     f"{protein_id}:p.{
+                                         protein[index]}{index + 1}=",
                                      f"{protein_id}:p.{seq3(protein[index])}{index + 1}=",))
+    # five prime untranslated region
+    for index in range(len(utr5)):
+        for base in unambiguous_dna_letters:
+            if base != utr5[index]:
+                variants.append((f"{seqrecord.id}:c.{index-len(utr5)}{utr5[index]}>{base}",
+                                 "",
+                                 "",))
+    # three prime untranslated region
+    for index in range(len(utr3)):
+        for base in unambiguous_dna_letters:
+            if base != utr3[index]:
+                variants.append((f"{seqrecord.id}:c.*{index+1}{utr3[index]}>{base}",
+                                 "",
+                                 "",))
     return variants
 
 
 def missense(gene: str) -> list:
     variants = []
-    handle = Entrez.esearch(db="nucleotide", term=f'{gene}[gene] "mane select"[keyword]')
+    handle = Entrez.esearch(db="nucleotide", term=f'{
+                            gene}[gene] "mane select"[keyword]')
     record = Entrez.read(handle)
-    handle = Entrez.efetch(db="nucleotide", id=record["IdList"], rettype="gb", retmode="text")
+    handle = Entrez.efetch(
+        db="nucleotide", id=record["IdList"], rettype="gb", retmode="text")
     seqrecord = SeqIO.read(handle, "genbank")
     for feature in seqrecord.features:
         if feature.type == "CDS":
@@ -80,9 +110,11 @@ def missense(gene: str) -> list:
 
 def mnv(gene: str) -> list:
     variants = []
-    handle = Entrez.esearch(db="nucleotide", term=f'{gene}[gene] "mane select"[keyword]')
+    handle = Entrez.esearch(db="nucleotide", term=f'{
+                            gene}[gene] "mane select"[keyword]')
     record = Entrez.read(handle)
-    handle = Entrez.efetch(db="nucleotide", id=record["IdList"], rettype="gb", retmode="text")
+    handle = Entrez.efetch(
+        db="nucleotide", id=record["IdList"], rettype="gb", retmode="text")
     seqrecord = SeqIO.read(handle, "genbank")
     for feature in seqrecord.features:
         if feature.type == "CDS":
@@ -96,35 +128,43 @@ def mnv(gene: str) -> list:
                 if protein[index] != seq.translate():
                     if base[0] == cds[codon] and base[1] == cds[codon + 1] and base[2] != cds[codon + 2]:
                         variants.append((f"{seqrecord.id}:c.{codon + 3}{cds[codon + 2]}>{base[2]}",
-                                         f"{protein_id}:p.{protein[index]}{index + 1}{seq.translate()}",
+                                         f"{protein_id}:p.{protein[index]}{
+                                             index + 1}{seq.translate()}",
                                          f"{protein_id}:p.{seq3(protein[index])}{index + 1}{seq3(seq.translate())}",))
                     elif base[0] == cds[codon] and base[1] != cds[codon + 1] and base[2] == cds[codon + 2]:
                         variants.append((f"{seqrecord.id}:c.{codon + 2}{cds[codon + 1]}>{base[1]}",
-                                         f"{protein_id}:p.{protein[index]}{index + 1}{seq.translate()}",
+                                         f"{protein_id}:p.{protein[index]}{
+                                             index + 1}{seq.translate()}",
                                          f"{protein_id}:p.{seq3(protein[index])}{index + 1}{seq3(seq.translate())}",))
                     elif base[0] != cds[codon] and base[1] == cds[codon + 1] and base[2] == cds[codon + 2]:
                         variants.append((f"{seqrecord.id}:c.{codon + 1}{cds[codon]}>{base[0]}",
-                                         f"{protein_id}:p.{protein[index]}{index + 1}{seq.translate()}",
+                                         f"{protein_id}:p.{protein[index]}{
+                                             index + 1}{seq.translate()}",
                                          f"{protein_id}:p.{seq3(protein[index])}{index + 1}{seq3(seq.translate())}",))
                     else:
                         variants.append((f"{seqrecord.id}:c.{codon + 1}_{codon + 3}{cds[codon:codon + 3]}>{base}",
-                                         f"{protein_id}:p.{protein[index]}{index + 1}{seq.translate()}",
+                                         f"{protein_id}:p.{protein[index]}{
+                                             index + 1}{seq.translate()}",
                                          f"{protein_id}:p.{seq3(protein[index])}{index + 1}{seq3(seq.translate())}",))
                 else:
                     if base[0] == cds[codon] and base[1] == cds[codon + 1] and base[2] != cds[codon + 2]:
                         variants.append((f"{seqrecord.id}:c.{codon + 3}{cds[codon + 2]}>{base[2]}",
-                                         f"{protein_id}:p.{protein[index]}{index + 1}=",
+                                         f"{protein_id}:p.{
+                                             protein[index]}{index + 1}=",
                                          f"{protein_id}:p.{seq3(protein[index])}{index + 1}=",))
                     elif base[0] == cds[codon] and base[1] != cds[codon + 1] and base[2] == cds[codon + 2]:
                         variants.append((f"{seqrecord.id}:c.{codon + 2}{cds[codon + 1]}>{base[1]}",
-                                         f"{protein_id}:p.{protein[index]}{index + 1}=",
+                                         f"{protein_id}:p.{
+                                             protein[index]}{index + 1}=",
                                          f"{protein_id}:p.{seq3(protein[index])}{index + 1}=",))
                     elif base[0] != cds[codon] and base[1] == cds[codon + 1] and base[2] == cds[codon + 2]:
                         variants.append((f"{seqrecord.id}:c.{codon + 1}{cds[codon]}>{base[0]}",
-                                         f"{protein_id}:p.{protein[index]}{index + 1}=",
+                                         f"{protein_id}:p.{
+                                             protein[index]}{index + 1}=",
                                          f"{protein_id}:p.{seq3(protein[index])}{index + 1}=",))
                     else:
                         variants.append((f"{seqrecord.id}:c.{codon + 1}_{codon + 3}{cds[codon:codon + 3]}>{base}",
-                                     f"{protein_id}:p.{protein[index]}{index + 1}=",
-                                     f"{protein_id}:p.{seq3(protein[index])}{index + 1}=",))
+                                         f"{protein_id}:p.{
+                                             protein[index]}{index + 1}=",
+                                         f"{protein_id}:p.{seq3(protein[index])}{index + 1}=",))
     return variants
